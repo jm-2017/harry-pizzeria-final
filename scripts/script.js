77bb0cbae827174125d1ad2abc0bd2190cd08c3a
Janet A. Martinez <149624779+jm-2017@users.noreply.github.com>
2024-12-02 23:43:44 -0600

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,81 @@
+$(()=> {
+// Create this to Speak to the console
+ console.log( "ready!" );
+ 
+ $('#btnSubmit').on('click', (event)=> {
+   event.preventDefault(); 
+  
+   console.log("Customer Full Name:", $('#fname').val());  
+   console.log("Customer address:", $('#address').val());  
+   
+   console.log("Selected Pizza Size:",
+    $(`#pizzaSize`).find(":selected").text()); 
+   console.log("Selected Pizza Type:",
+    $(`#pizzaType`).find(":selected").text()); 
+   
+   if($('#checkPep').is(':checked')){
+     console.log("Checkbox Pep Value:", $('#checkPep').val()); 
+   }
+    if($('#checkOnion').is(':checked')){
+     console.log("Checkbox Onion Value:", $('#checkOnion').val()); 
+   }
+    if($('#checkJal').is(':checked')){
+     console.log("Checkbox Jalapeno Value:", $('#checkJal').val()); 
+   }
+    if($('#checkPine').is(':checked')){
+     console.log("Checkbox Pineapple Value:", $('#checkPine').val()); 
+   }
+    if($('#checkShrooms').is(':checked')){
+     console.log("Checkbox Mushrooms Value:", $('#checkShrooms').val()); 
+   }
+    if($('#checkOlives').is(':checked')){
+     console.log("Checkbox Olives Value:", $('#checkOlives').val()); 
+   }
+    if($('#checkSix').is(':checked')){
+     console.log("Checkbox Six pc wings Value:", $('#checkSix').val()); 
+   }
+    if($('#checkTwelve').is(':checked')){
+     console.log("Checkbox Twelve pc wings Value:", $('#checkTwelve').val()); 
+   }
+   
+    console.log("Selected Bone-in flavor:",  $('#boneInFlavor').find(":selected").text());
+    console.log("Selected Boneless flavor:",  $('#bonelessFlavor').find(":selected").text());
+    console.log("Selected Drink choice:",  $('#inputDrink').find(":selected").text());
+  })
+});
+
+$(function () {
+  //what radio value did they select?
+
+  $("#formSubmit").on("click", (e) => {
+    e.preventDefault();
+    });
+      
+  
+  $("input[type=radio]").on("change", function () {
+    let radioChoice = $("input[type=radio]:checked").val(); // A or B
+
+    if (radioChoice === "Pizza") {
+      $("#pizzaOption")
+      $("#pizzaSize")
+        .empty()
+        .append(`<option value="Choose size"> Choose Size </option>`)
+        .append(`<option value="Large"> Large </option>`)
+        .append(`<option value="Medium"> Medium </option>`)
+        .append(`<option value="Small"> Small </option>`);
+      
+      $("#pizzaType")
+        .empty()
+        .append(`<option value="Choose pizza type"> Choose pizza type </option>`)
+        .append(`<option value="Classic"> Classic </option>`)
+        .append(`<option value="Hawaiian"> Hawaiian </option>`)
+        .append(`<option value="Roman style"> Roman style </option>`);
+     
+    } 
+    else {
+      $("#foodOption")
+        .empty()
+        .append(`<option value=""> Pick your food choice </option>`);
+    }
+  });
+});
